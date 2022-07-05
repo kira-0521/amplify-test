85bf9bc0d40066ad3d9a3d65a0a79bd95ceee6c5
kira-0521 <kiratanaka0521@gmail.com>
2022-07-05 21:37:13 +0900

    [ add ] recaptcha用hooks

--- src/lib/hooks/useRecaptcha.ts
+++ src/lib/hooks/useRecaptcha.ts
@@ -3,22 +3,22 @@ import { useCallback } from 'react'
 const ID = 'google-recaptcha-v3' // script タグのIDとして使用する任意の文字列
 const SITE_KEY = import.meta.env.RECAPTCHA_SITE_KEY // reCAPTCHA の site key
 
-export type UseReCaptcha = {
+export type UseReCaptcha = [
   /**
    * reCAPTCHA の読み込みを非同期で行う
    */
-  load: () => void
+  load: () => void,
 
   /**
    * reCAPTCHA の読み込みが完了するのを待つ
    */
-  ready: () => Promise<ReCaptchaV2.ReCaptcha>
+  ready: () => Promise<ReCaptchaV2.ReCaptcha>,
 
   /**
    * reCAPTCHA を実行する
    */
   execute: (action: ReCaptchaV2.Action) => Promise<string>
-}
+]
 
 export const useRecaptcha = (): UseReCaptcha => {
   // reCaptchaのためのscriptタグを読み込む
@@ -36,5 +36,33 @@ export const useRecaptcha = (): UseReCaptcha => {
     head.appendChild(script)
   }, [])
 
-  const reacy = useCallback(() => {}, [])
+  const ready = useCallback(() => {
+    return new Promise<ReCaptchaV2.ReCaptcha>((resolve) => {
+      load()
+
+      if (typeof window.grecaptcha.ready === 'undefined') {
+        window.grecaptcha = window.grecaptcha || {}
+        window.grecaptcha.ready = (cb: () => void) => {
+          // @ts-ignore
+          window.___grecaptcha_cfg ??= {}
+          // @ts-ignore
+          window.___grecaptcha_cfg.fns ??= []
+          // @ts-ignore
+          window.___grecaptcha_cfg.fns.push(cb)
+        }
+      }
+
+      window.grecaptcha.ready(() => resolve(window.grecaptcha))
+    })
+  }, [load])
+
+  const execute = useCallback(
+    async (action: ReCaptchaV2.Action): Promise<string> => {
+      const grecaptcha = await ready()
+      return grecaptcha.execute(SITE_KEY, action)
+    },
+    [ready]
+  )
+
+  return [load, ready, execute]
 }
